40444c4f358e8c24a593725d068922fb4239b1db
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-11-14 10:07:14 -0300

    refactor: adicionando url da api de java deployada

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api'; 
-
-//const API_BASE_URL = 'https://challenge-mottu-java-mobile.onrender.com'; 
+// const API_BASE_URL = 'http://localhost:8080/api'; 
+const API_BASE_URL = 'https://gs-java-0em0.onrender.com/api'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
